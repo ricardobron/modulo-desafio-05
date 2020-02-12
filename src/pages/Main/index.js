253b6bb4f3f9e7e0879dac9619397d796b958998
Ricardo <ricardoslv688@gmail.com>
2020-02-12 16:18:58 +0000

    Desafio 5 completo

---
+++ src/pages/Main/index.js
@@ -0,0 +1,123 @@
+/* eslint-disable react/no-unused-state */
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import Container from '../../components/Container';
+import { Form, SubmitButton, List, MessageErrors } from './styles';
+
+export default class Main extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+    error: false,
+    errorMessages: '',
+  };
+
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value, errorMessages: '' });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    try {
+      if (newRepo === '') {
+        this.setState({
+          errorMessages: 'É necessário indicar um repositório.',
+        });
+
+        throw new Error();
+      }
+
+      const hasRepo = repositories.find(repo => repo.name === newRepo);
+
+      if (hasRepo) {
+        this.setState({ errorMessages: 'O repositório informado já existe.' });
+
+        throw new Error('Repositório duplicado');
+      }
+
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+      });
+    } catch (err) {
+      setTimeout(() => {
+        this.setState({ error: true, errorMessages: '' });
+      }, 3000);
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { newRepo, repositories, loading, error, errorMessages } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit} error={error}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+        <List>
+          <MessageErrors>{errorMessages}</MessageErrors>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
